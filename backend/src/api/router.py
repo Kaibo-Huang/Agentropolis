eb3372c231328a24950e6c65c61b977b524e9cdd
349lolol <PatrickZX.wei@gmail.com>
2026-03-14 20:56:56 -0400

    somewhat working voronoi regions

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -6,6 +6,7 @@ from src.api.followers import router as followers_router
 from src.api.posts import router as posts_router
 from src.api.sessions import router as sessions_router
 from src.api.tick import router as tick_router
+from src.api.zones import router as zones_router
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(sessions_router)
@@ -14,3 +15,4 @@ api_router.include_router(events_router)
 api_router.include_router(followers_router)
 api_router.include_router(posts_router)
 api_router.include_router(archetypes_router)
+api_router.include_router(zones_router)
